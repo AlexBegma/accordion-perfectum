363eba854349763aa3e0ff5f2142aff8c5a6054d
alexbehma <alexbegma@gmail.com>
2019-09-20 12:29:25 +0300

    created js-file, first step pop-up

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("readAll").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("readAll").style.display = "none";
+}
